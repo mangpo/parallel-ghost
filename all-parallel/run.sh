23b7edea39f96bff792f4a3a8717a38f323f14ae
mangpo <mangpo@eecs.berkeley.edu>
2013-11-24 22:40:29 -0800

    edit shell script

--- all-parallel/run.sh
+++ all-parallel/run.sh
@@ -1,6 +1,6 @@
-time python main_popen.py out/proc4_1.csv 4 > out/proc4_1.log
-time python main_popen.py out/proc4_2.csv 4 > out/proc4_2.log
-time python main_popen.py out/proc4_3.csv 4 > out/proc4_3.log
-time python main_popen.py out/proc1_1.csv 1 > out/proc1_1.log
-time python main_popen.py out/proc8_1.csv 8 > out/proc8_1.log
+time python main_popen.py out/proc4_1.csv 4 > out/proc4_1.log 2>&1
+time python main_popen.py out/proc4_2.csv 4 > out/proc4_2.log 2>&1
+time python main_popen.py out/proc4_3.csv 4 > out/proc4_3.log 2>&1
+time python main_popen.py out/proc1_1.csv 1 > out/proc1_1.log 2>&1
+time python main_popen.py out/proc8_1.csv 8 > out/proc8_1.log 2>&1
 sh run2.sh
